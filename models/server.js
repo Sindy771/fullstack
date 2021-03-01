382c87dc4e1dbf293701483af0d9aa08e453f06d
unknown <sindymilenasalamanca24@gmail.com>
2021-03-01 14:18:53 -0500

    tarea

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dbConection from "../database/config.js";
+import categoria from '../routes/categoria.js'
 
 class Server {
   constructor() {
@@ -15,7 +16,9 @@ class Server {
     this.routes();
   }
 
-  routes() {}
+  routes() {
+    this.app.use('/api/categoria', categoria )
+  }
 
   async dbConexion() {
     await dbConection();
@@ -29,11 +32,9 @@ class Server {
 
   listen() {
     this.app.listen(this.port, () => {
-      console.log(
-        `Servidor corriendo en el puerto ${this.port}`
-      );
+      console.log(`Servidor corriendo en el puerto ${this.port}`);
     });
   }
 }
 
-export {Server};
+export { Server };
